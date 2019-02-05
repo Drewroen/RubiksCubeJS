11db3914a82db0b98b6bf3793ed26b8f22ad8d03
Drewroen <drewroen@gmail.com>
2019-02-05 11:17:14 -0600

    Added a front and x rotation. Will be adding y and z next.

--- js/Gui.js
+++ js/Gui.js
@@ -1,20 +1,20 @@
 window.onload = function() {
   var gui = new dat.GUI();
   var rubiksMovements = {
+    front:function(){front(rubiksCubeFaces);},
+    frontPrime:function(){frontPrime(rubiksCubeFaces);},
     right:function(){console.log("Right")},
     rightPrime:function(){console.log("Right Prime")},
     left:function(){console.log("Left")},
     leftPrime:function(){console.log("Left Prime")},
-    front:function(){console.log("Front"); front(rubiksCubeFaces);},
-    frontPrime:function(){console.log("Front Prime");},
     back:function(){console.log("Back")},
     backPrime:function(){console.log("Back Prime")},
     up:function(){console.log("Up")},
     upPrime:function(){console.log("Up Prime")},
     down:function(){console.log("Down")},
     downPrime:function(){console.log("Down Prime")},
-    xAxis:function(){console.log("X Axis")},
-    xAxisPrime:function(){console.log("X Axis Prime")},
+    xAxis:function(){xAxis(rubiksCubeFaces);},
+    xAxisPrime:function(){xAxisPrime(rubiksCubeFaces);},
     yAxis:function(){console.log("Y Axis")},
     yAxisPrime:function(){console.log("Y Axis Prime")},
     zAxis:function(){console.log("Z Axis")},
@@ -28,4 +28,7 @@ window.onload = function() {
   };
 
   gui.add(rubiksMovements, 'front').name('F');
+  gui.add(rubiksMovements, 'frontPrime').name('F\'');
+  gui.add(rubiksMovements, 'xAxis').name('X');
+  gui.add(rubiksMovements, 'xAxisPrime').name('X\'');
 };
--- js/RubiksCubeRotations.js
+++ js/RubiksCubeRotations.js
@@ -6,9 +6,89 @@ function front(rubiksCube)
     var coordToRotate = ((2 - (i % 3)) * 3) + Math.floor(i / 3);
     setCubeFace(rubiksCube, 0, i, getCubeFace(rubiksCubeOldReference, 0, coordToRotate));
   }
+
+  for(var i = 0; i < 3; i++)
+  {
+    setCubeFace(rubiksCube, 1, (3 * i) + 2, getCubeFace(rubiksCubeOldReference, 4, (3 * (3 - i)) - 1));
+    setCubeFace(rubiksCube, 5, (3 * i) + 2, getCubeFace(rubiksCubeOldReference, 1, (3 * i) + 2));
+    setCubeFace(rubiksCube, 3, (3 * (3 - i)) - 1, getCubeFace(rubiksCubeOldReference, 5, (3 * i) + 2));
+    setCubeFace(rubiksCube, 4, (3 * (3 - i)) - 1, getCubeFace(rubiksCubeOldReference, 3, (3 * (3 - i)) - 1));
+  }
 }
 
-function frontPrime(rubiksCubeToTurn)
+function frontPrime(rubiksCube)
+{
+  front(rubiksCube);
+  front(rubiksCube);
+  front(rubiksCube);
+}
+
+function xAxis(rubiksCube)
+{
+  rubiksCubeOldReference = createCubeFacesFromCube(rubiksCube);
+  setCubeFace(rubiksCube, 4, 0, getCubeFace(rubiksCubeOldReference, 0, 2));
+  setCubeFace(rubiksCube, 4, 1, getCubeFace(rubiksCubeOldReference, 0, 1));
+  setCubeFace(rubiksCube, 4, 2, getCubeFace(rubiksCubeOldReference, 0, 0));
+  setCubeFace(rubiksCube, 4, 3, getCubeFace(rubiksCubeOldReference, 0, 5));
+  setCubeFace(rubiksCube, 4, 4, getCubeFace(rubiksCubeOldReference, 0, 4));
+  setCubeFace(rubiksCube, 4, 5, getCubeFace(rubiksCubeOldReference, 0, 3));
+  setCubeFace(rubiksCube, 4, 6, getCubeFace(rubiksCubeOldReference, 0, 8));
+  setCubeFace(rubiksCube, 4, 7, getCubeFace(rubiksCubeOldReference, 0, 7));
+  setCubeFace(rubiksCube, 4, 8, getCubeFace(rubiksCubeOldReference, 0, 6));
+  setCubeFace(rubiksCube, 0, 0, getCubeFace(rubiksCubeOldReference, 5, 0));
+  setCubeFace(rubiksCube, 0, 1, getCubeFace(rubiksCubeOldReference, 5, 1));
+  setCubeFace(rubiksCube, 0, 2, getCubeFace(rubiksCubeOldReference, 5, 2));
+  setCubeFace(rubiksCube, 0, 3, getCubeFace(rubiksCubeOldReference, 5, 3));
+  setCubeFace(rubiksCube, 0, 4, getCubeFace(rubiksCubeOldReference, 5, 4));
+  setCubeFace(rubiksCube, 0, 5, getCubeFace(rubiksCubeOldReference, 5, 5));
+  setCubeFace(rubiksCube, 0, 6, getCubeFace(rubiksCubeOldReference, 5, 6));
+  setCubeFace(rubiksCube, 0, 7, getCubeFace(rubiksCubeOldReference, 5, 7));
+  setCubeFace(rubiksCube, 0, 8, getCubeFace(rubiksCubeOldReference, 5, 8));
+
+  setCubeFace(rubiksCube, 5, 0, getCubeFace(rubiksCubeOldReference, 2, 2));
+  setCubeFace(rubiksCube, 5, 1, getCubeFace(rubiksCubeOldReference, 2, 1));
+  setCubeFace(rubiksCube, 5, 2, getCubeFace(rubiksCubeOldReference, 2, 0));
+  setCubeFace(rubiksCube, 5, 3, getCubeFace(rubiksCubeOldReference, 2, 5));
+  setCubeFace(rubiksCube, 5, 4, getCubeFace(rubiksCubeOldReference, 2, 4));
+  setCubeFace(rubiksCube, 5, 5, getCubeFace(rubiksCubeOldReference, 2, 3));
+  setCubeFace(rubiksCube, 5, 6, getCubeFace(rubiksCubeOldReference, 2, 8));
+  setCubeFace(rubiksCube, 5, 7, getCubeFace(rubiksCubeOldReference, 2, 7));
+  setCubeFace(rubiksCube, 5, 8, getCubeFace(rubiksCubeOldReference, 2, 6));
+
+  setCubeFace(rubiksCube, 2, 0, getCubeFace(rubiksCubeOldReference, 4, 0));
+  setCubeFace(rubiksCube, 2, 1, getCubeFace(rubiksCubeOldReference, 4, 1));
+  setCubeFace(rubiksCube, 2, 2, getCubeFace(rubiksCubeOldReference, 4, 2));
+  setCubeFace(rubiksCube, 2, 3, getCubeFace(rubiksCubeOldReference, 4, 3));
+  setCubeFace(rubiksCube, 2, 4, getCubeFace(rubiksCubeOldReference, 4, 4));
+  setCubeFace(rubiksCube, 2, 5, getCubeFace(rubiksCubeOldReference, 4, 5));
+  setCubeFace(rubiksCube, 2, 6, getCubeFace(rubiksCubeOldReference, 4, 6));
+  setCubeFace(rubiksCube, 2, 7, getCubeFace(rubiksCubeOldReference, 4, 7));
+  setCubeFace(rubiksCube, 2, 8, getCubeFace(rubiksCubeOldReference, 4, 8));
+}
+
+function xAxisPrime(rubiksCube)
+{
+  xAxis(rubiksCube);
+  xAxis(rubiksCube);
+  xAxis(rubiksCube);
+}
+
+function yAxis(rubiksCubeToTurn)
+{
+
+}
+
+function yAxisPrime(rubiksCubeToTurn)
+{
+
+}
+
+function zAxis(rubiksCubeToTurn)
+{
+
+}
+
+function zAxisPrime(rubiksCubeToTurn)
 {
 
 }
@@ -89,36 +169,6 @@ function standing(rubiksCubeToTurn)
 }
 
 function standingPrime(rubiksCubeToTurn)
-{
-
-}
-
-function xAxis(rubiksCubeToTurn)
-{
-
-}
-
-function xAxisPrime(rubiksCubeToTurn)
-{
-
-}
-
-function yAxis(rubiksCubeToTurn)
-{
-
-}
-
-function yAxisPrime(rubiksCubeToTurn)
-{
-
-}
-
-function zAxis(rubiksCubeToTurn)
-{
-
-}
-
-function zAxisPrime(rubiksCubeToTurn)
 {
 
 }
